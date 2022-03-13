d3b5da3112e80fabd7cc569cb1367671765cebb4
thismrm <achmadnassardhi@gmail.com>
2022-03-13 19:08:30 +0700

    feat: integrate with deta.sh

---
+++ deta_liftoff/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { Deta } = require("deta");
+
+// deta setup
+const deta = Deta(process.env.PROJECT_KEY);
+const db = deta.Base(process.env.PROJECT_DB);
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>
+  res.render("index", {
+    email: false,
+  })
+);
+
+app.post("/success", async (req, res) => {
+  await db.put({ email: req.body.email });
+  res.render("index", { email: true });
+});
+
+if (process.env.DETA_PATH === "true") {
+  module.exports = app;
+} else {
+  app.listen(3000, () => console.log("running on local server"));
+}
